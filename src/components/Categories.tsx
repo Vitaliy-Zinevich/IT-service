dc1f4568bd9af4fca5ce4a3401ddde10fc2fcbcc
Vitaliy-Zinevich <everwoods21011993@gmail.com>
2023-06-14 19:45:56 +0300

    header

---
+++ src/components/Categories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const categories = ['Программирование', 'Дизайн', 'Игры'];
+
+const Categories: React.FC = () => {
+  return (
+    <div>Categories</div>
+  )
+}
+
+export default Categories;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 
-function Header() {
+const Header: React.FC = () => {
   return (
     <div className='header'>
         <div className="container">
-            <div className="header__logo">
-                 <img  src="https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="It-service logo" />
-            <div>
-            </div>
-            </div>
-            <div className="header__home">Home</div>
-            <div className="header__news">News</div>
-            <div className="header__source">Source</div>
-            <div className="header__login">Login</div>
-            <div className="header__account">Account</div>
+            <div className="header__logo">IT-service</div>
+            <div className="header__home">Главная</div>
+            <div className="header__news">Новости</div>
+            <div className="header__account">Войти</div>
         </div>
     </div>
   )
