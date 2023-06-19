e8820d302aaf6487c9f7fd0f28f966babb73b5c8
Vitaliy-Zinevich <everwoods21011993@gmail.com>
2023-06-19 18:07:13 +0300

    info block

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -3,8 +3,15 @@ import React from 'react'
 const Info: React.FC = () => {
   return (
     <div className='info'>
-        <div className="info_text">Текст</div>
-        <div className="info_imade">Картинка</div>
+        <div className="info_text">
+            <h1>Плюсы работы в IT</h1>
+             <p>- Востребованность на рынке труда</p>
+             <p>- Возможности для карьерного роста</p>
+             <p>- Высокие зарплаты</p>
+             <p>- Постоянное обучение и развитие</p>
+             <p>- Интересная работа</p>
+        </div>
+        <div className="info_image">Картинка</div>
     </div>
   )
 }
